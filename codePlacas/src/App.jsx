70b4c1e24d6c93063ad3dc7f855cf918f39a1f83
RendonSofia1 <rendonsofi03@gmail.com>
2025-05-08 18:00:08 -0600

    webcam

--- codePlacas/src/App.jsx
+++ codePlacas/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Loadvideo from "./pages/LoadVideo.jsx";
 import Search from "./pages/Search.jsx";
@@ -7,7 +7,7 @@ import Search from "./pages/Search.jsx";
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen max-h-full bg-zinc-950 text-white">
+      <div className=" bg-zinc-950 text-white">
         <nav className="p-4 bg-zinc-800">
           <ul className="flex space-x-4">
             <li>
@@ -24,6 +24,7 @@ export default function App() {
 
         <div className="p-4">
           <Routes>
+            <Route path="*" element={<Navigate to="/" />} />
             <Route path="/" element={<Home />} />
             <Route path="/loadvideo" element={<Loadvideo />} />
             <Route path="/search" element={<Search />} />
--- codePlacas/src/pages/Home.jsx
+++ codePlacas/src/pages/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Webcam from "react-webcam";
+import Alert from "@mui/material/Alert";
 
 export default function Home() {
-  return <div className="text-white">Bienvenido a la pantalla de inicio</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Webcam
+        audio={false}
+        videoConstraints={{ facingMode: "user" }}
+        className="rounded-xl w-100 h-auto"
+      />
+    </div>
+  );
 }
